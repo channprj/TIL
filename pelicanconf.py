19c8bee7a3bf07a4d2b78e91f2df0381080055a7
CHANN <chann@chann.kr>
2016-01-28 00:27:16 +0900

    Change theme.

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,23 +2,35 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+
+### Site Setting
 AUTHOR = 'CHANN'
 SITENAME = 'Today I Learned'
 SITESUBTITLE = 'Novice Programmer'
 SITEURL = 'http://til.chann.kr'
 # SITEURL = 'http://localhost:8000'
 DISQUS_SITENAME = u'githubs'
-
 # DISQUS_SECRET_KEY = u'YOUR_SECRET_KEY'
 # DISQUS_PUBLIC_KEY = u'YOUR_PUBLIC_KEY'
+# GOOGLE_ANALYTICS = ""
+# GITHUB_USER = 'channprj'
+
 
 ### THEME SETTING
-THEME = 'theme/pelican-svbhack'
-# THEME = 'theme/replika'
-# BOOTSTRAP_NAVBAR_INVERSE = True
-# DISPLAY_CATEGORY_IN_BREADCRUMBS = True
-# COVER_IMG_URL = 'http://blog.chann.kr/content/images/2015/11/photo-1421757295538-9c80958e75b0-1.jpeg'
+# THEME = 'theme/pelican-svbhack'
+THEME = 'theme/peliwiki'
 
+
+BOOTSTRAP_NAVBAR_INVERSE = True
+DISPLAY_CATEGORY_IN_BREADCRUMBS = True
+DISPLAY_CATEGORIES_ON_MENU = False
+DISPLAY_BREADCRUMBS = True
+DISPLAY_CATEGORY_IN_BREADCRUMBS = True
+HIDE_SIDEBAR = True
+MENUITEMS = (
+    ('About', '/about'),
+    ('Blog', 'https://blog.chann.kr'),
+)
 
 OUTPUT_PATH = 'output'
 PATH = 'content'
@@ -50,16 +62,18 @@ SOCIAL = (
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
 
-# Remove html extension
+### Remove html extension
 ARCHIVE_URL = '{archive}'
+ARCHIVE_SAVE_AS = ARCHIVE_URL+'.html'
 
 ARTICLE_URL = '{category}/{slug}'
 ARTICLE_SAVE_AS = ARTICLE_URL+'.html'
 
 PAGE_URL = '{slug}'
-PAGE_SAVE_AS = PAGE_URL+'.html'
+# PAGE_SAVE_AS = PAGE_URL+'.html'
+PAGE_SAVE_AS = PAGE_URL+'/index.html'  # Multi Page
 
 CATEGORY_URL = '{slug}/index'
 CATEGORY_SAVE_AS = CATEGORY_URL+'.html'
